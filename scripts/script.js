9d1ef64936eccf1d99441d95f84967ccfd0cfed0
knox <lux.barker13@gmail.com>
2021-02-05 19:34:25 -0800

    Create basic operate function

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,23 @@ const multiply = function multiplyNumbers(num1, num2) {
 const divide = function divideNumbers(num1, num2) {
   return num1 / num2;
 };
+
+const operate = function performOperation(num1, operator, num2) {
+  let result;
+  switch (operator) {
+    case '+':
+      result = add(num1, num2);
+      break;
+    case '-':
+      result = subtract(num1, num2);
+      break;
+    case '*':
+      result = multiply(num1, num2);
+      break;
+    case '/':
+      result = divide(num1, num2);
+      break;
+    default: // nothing happens
+  }
+  return result;
+};
