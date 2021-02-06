1a66e7622d7ac3b6dcb0c873301d6b3d992f9435
knox <lux.barker13@gmail.com>
2021-02-05 19:17:55 -0800

    Initial basic math functions

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const add = function addNumbers(num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function subtractNumbers(num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function multiplyNumbers(num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function divideNumbers(num1, num2) {
+  return num1 / num2;
+};
